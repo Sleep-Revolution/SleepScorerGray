8335dbad71451ad336b61f9dcb6e6930d1eca199
gabjou <87657702+gabjou@users.noreply.github.com>
2023-02-15 16:17:39 +0000

    Add git ignore an requirement change

--- api/main.py
+++ api/main.py
@@ -2,12 +2,22 @@ from fastapi import FastAPI, UploadFile
 from fastapi.responses import FileResponse
 import time
 from random import randint
+from pathlib import Path
+import os
+print(os.environ['nox_url'])
+
+x = 12308
+y = 12093812
+z = (x + y) & 1
+if z == 1:
+    print("foo")
 
 app = FastAPI()
 
 @app.post('/uploadfile')
 async def create_upload_file(file: UploadFile):
-    file_location = f'files/{file.filename}'
+    file_location = os.path.join(os.path.expanduser("~"),f'/files/{file.filename}')
+    print(file_location)
     with open(file_location, 'wb+') as file_object:
         file_object.write(file.file.read())
     #return {'info': f"file '{file.filename}' saved at '{file_location}'"}
