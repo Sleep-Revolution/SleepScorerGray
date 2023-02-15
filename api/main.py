56475978149ea777cfdce2c7135218d18de7d2a2
gabjou <87657702+gabjou@users.noreply.github.com>
2023-02-15 16:33:06 +0000

    Update main.py

--- api/main.py
+++ api/main.py
@@ -4,20 +4,12 @@ import time
 from random import randint
 from pathlib import Path
 import os
-print(os.environ['nox_url'])
-
-x = 12308
-y = 12093812
-z = (x + y) & 1
-if z == 1:
-    print("foo")
 
 app = FastAPI()
 
 @app.post('/uploadfile')
 async def create_upload_file(file: UploadFile):
     file_location = os.path.join(os.path.expanduser("~"),f'/files/{file.filename}')
-    print(file_location)
     with open(file_location, 'wb+') as file_object:
         file_object.write(file.file.read())
     #return {'info': f"file '{file.filename}' saved at '{file_location}'"}
